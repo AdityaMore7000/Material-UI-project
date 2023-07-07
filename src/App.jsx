2635466fbbbc5a661354a0bdf6a26944ddaf0bca
AdityaMore7000 <adi.more7000@gmail.com>
2023-07-07 17:26:28 +0530

    video 8

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,28 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import { purple } from "@mui/material/colors";
 function App() {
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main:'#9c27b0'
+      },
+      secondary: purple
+    },
+    typography:{
+      fontFamily:" 'Quicksand', sans-serif"
+    }
+  })
   return (
     <>
+    <ThemeProvider theme={theme}>
     <Routes>
       <Route index element={<Notes />} />
       <Route path="/create" element={<Create />} />
     </Routes>
+    </ThemeProvider>
     </>
   )
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,22 +1,77 @@
 import Typography from "@mui/material/Typography";
-import Button from '@mui/material/Button';
-import { Container } from "@mui/material";
+import Button from "@mui/material/Button";
+import { Container, FormControlLabel, Radio, TextField } from "@mui/material"; 
+import SendIcon from "@mui/icons-material/Send";
+import { useState } from "react";
+import RadioGroup from '@mui/material/RadioGroup';
 export default function Create() {
+  const [title, setTitle] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [details, setDetails] = useState('')
+  const [category,setCategory] = useState('work')
+  const [detailsError, setDetailsError] = useState(false)
   return (
-    <Container>
-    <Typography
-    variant={"h6"}
-    component={"h2"}
-    gutterBottom
-    color="textSecondary"
-    >
+    <Container maxWidth="xl">
+      <Typography
+        variant={"h6"}
+        component={"h2"}
+        gutterBottom
+        color="textSecondary"
+      >
         Create a New Note
-    </Typography>
+      </Typography>
 
-    <Button type="submit" color="secondary" variant="contained" onClick={()=>{
-        console.log('You submitted');
-    }}>Submit</Button>
-    {/* <Button variant="contained" type="submit">Submit</Button>
+      <form noValidate autoComplete="off"
+      onSubmit={(e)=>{
+        e.preventDefault();
+        if(title=='') setTitleError(true)
+        else setTitleError(false)
+        if(details=='') setDetailsError(true)
+        else setDetailsError(false)
+        if(title&&details){
+          console.log(title);
+          console.log(details);
+        }
+        console.log(category);
+      }}>
+        <TextField
+          sx={{ margin: "20px 0", display: "block" }}
+          label="Note Title"
+          variant="outlined"
+          onChange={(e)=>setTitle(e.target.value)}
+          color="secondary"
+          fullWidth
+          error={titleError}
+          required
+          />
+        <TextField
+          sx={{ margin: "20px 0", display: "block" }}
+          label="Details"
+          onChange={(e)=>setDetails(e.target.value)}
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          error={detailsError}
+          multiline
+          rows={4}
+          required
+        />
+      <RadioGroup value={category} name="use-radio-group" defaultValue="first" onChange={(e)=>setCategory(e.target.value)}>
+        <FormControlLabel label="Money" value="money" control={<Radio/>}/>
+        <FormControlLabel label="Todos" value="todos" control={<Radio/>}/>
+        <FormControlLabel label="Reminders" value="reminders" control={<Radio/>}/>
+        <FormControlLabel label="Work" value="work" control={<Radio/>}/>
+      </RadioGroup>
+      <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<SendIcon />}
+        >
+        Send
+      </Button>
+        </form>
+      {/* <Button variant="contained" type="submit">Submit</Button>
     <br />
     <br />
     <Button variant="outlined"color="secondary">Submit</Button>
