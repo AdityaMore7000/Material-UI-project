99cd95293c065f1bbe3c1aec7faef1d1b7441b69
AdityaMore7000 <adi.more7000@gmail.com>
2023-07-08 10:32:28 +0530

    video 11

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,15 +1,19 @@
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Container, FormControlLabel, Radio, TextField } from "@mui/material"; 
+import { Container, FormControlLabel, Radio, TextField } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useState } from "react";
-import RadioGroup from '@mui/material/RadioGroup';
+import RadioGroup from "@mui/material/RadioGroup";
+import { FormLabel, FormControl } from "@mui/material";
+import { useNavigate } from 'react-router-dom'
+
 export default function Create() {
-  const [title, setTitle] = useState('')
-  const [titleError, setTitleError] = useState(false)
-  const [details, setDetails] = useState('')
-  const [category,setCategory] = useState('work')
-  const [detailsError, setDetailsError] = useState(false)
+  const navigate = useNavigate()
+  const [title, setTitle] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState("work");
+  const [detailsError, setDetailsError] = useState(false);
   return (
     <Container maxWidth="xl">
       <Typography
@@ -21,33 +25,38 @@ export default function Create() {
         Create a New Note
       </Typography>
 
-      <form noValidate autoComplete="off"
-      onSubmit={(e)=>{
-        e.preventDefault();
-        if(title=='') setTitleError(true)
-        else setTitleError(false)
-        if(details=='') setDetailsError(true)
-        else setDetailsError(false)
-        if(title&&details){
-          console.log(title);
-          console.log(details);
-        }
-        console.log(category);
-      }}>
+      <form
+        noValidate
+        autoComplete="off"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (title == "") setTitleError(true);
+          else setTitleError(false);
+          if (details == "") setDetailsError(true);
+          else setDetailsError(false);
+          if (title && details) {
+            fetch("http://localhost:8000/notes", {
+              method: "POST",
+              headers: { "Content-type": "application/json" },
+              body: JSON.stringify({ title, details, category }),
+            }).then(()=>navigate('/'))
+          }
+        }}
+      >
         <TextField
           sx={{ margin: "20px 0", display: "block" }}
           label="Note Title"
           variant="outlined"
-          onChange={(e)=>setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
           color="secondary"
           fullWidth
           error={titleError}
           required
-          />
+        />
         <TextField
           sx={{ margin: "20px 0", display: "block" }}
           label="Details"
-          onChange={(e)=>setDetails(e.target.value)}
+          onChange={(e) => setDetails(e.target.value)}
           variant="outlined"
           color="secondary"
           fullWidth
@@ -56,21 +65,33 @@ export default function Create() {
           rows={4}
           required
         />
-      <RadioGroup value={category} name="use-radio-group" defaultValue="first" onChange={(e)=>setCategory(e.target.value)}>
-        <FormControlLabel label="Money" value="money" control={<Radio/>}/>
-        <FormControlLabel label="Todos" value="todos" control={<Radio/>}/>
-        <FormControlLabel label="Reminders" value="reminders" control={<Radio/>}/>
-        <FormControlLabel label="Work" value="work" control={<Radio/>}/>
-      </RadioGroup>
-      <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<SendIcon />}
-        >
-        Send
-      </Button>
-        </form>
+        <FormControl>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            name="use-radio-group"
+            defaultValue="first"
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel label="Money" value="money" control={<Radio />} />
+            <FormControlLabel label="Todos" value="todos" control={<Radio />} />
+            <FormControlLabel
+              label="Reminders"
+              value="reminders"
+              control={<Radio />}
+            />
+            <FormControlLabel label="Work" value="work" control={<Radio />} />
+          </RadioGroup>
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<SendIcon />}
+          >
+            Send
+          </Button>
+        </FormControl>
+      </form>
       {/* <Button variant="contained" type="submit">Submit</Button>
     <br />
     <br />
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,37 @@
+import { useEffect,useState } from "react"
+import {Paper, Grid} from '@mui/material'
+import { Container } from "@mui/material"
 export default function Notes(){
+    const [notes, setNotes] = useState([])
+    useEffect(()=>{
+        fetch('http://localhost:8000/notes')
+        .then(res =>res.json())
+        .then(data=>setNotes(data))
+    },[])
+
     return(
-        <>
-        <h1>Notes</h1>
-        </>
+        <Container>
+        {/* <Grid container gap={1}>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>1</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>2</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>3</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+                <Paper>4</Paper>
+            </Grid>
+        </Grid> */}
+        <Grid container gap={1}>
+        {notes.map(note=>{
+            return(
+                <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key = {note.id}><Paper>{note.id}</Paper></Grid> 
+                )
+            })}
+        </Grid>
+        </Container>
     )
 }
